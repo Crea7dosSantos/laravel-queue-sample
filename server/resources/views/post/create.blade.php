275d5ba6c83641adc9ba601798eaa9d4589dfe91
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-09-29 05:19:54 +0900

    close #7

--- server/resources/views/post/create.blade.php
+++ server/resources/views/post/create.blade.php
@@ -4,8 +4,32 @@
 <div>
     <h1 class="text-6xl">Create Post</h1>
 
-    <div class="mt-10">
-        <a class="bg-gray-600 text-white text-base p-2 rounded-md" href="{{ route('post.create') }}">Create post</a>
+    @if (count($errors) != 0)
+    <div class="pt-4">
+        <h3 class="text-lg text-red-400 py-3">Faild create post</h3>
+        @foreach ($errors->all() as $error)
+        <li class="pt-1">{{ $error }}</li>
+        @endforeach
     </div>
+    @endif
+
+    <form id='form' action="{{ route('post.store') }}" method="post">
+        @csrf
+        <div class="mt-10">
+            <label for="">Notification email address for post creation</label>
+            <input class="ml-3 border-2 border-blue-500 rounded" type="text" name="email">
+        </div>
+        <div class="mt-7">
+            <label for="">Post title</label>
+            <input class="ml-3 border-2 border-blue-500 rounded" type="text" name="title">
+        </div>
+        <div class="mt-7">
+            <label for="">Post content</label>
+            <input class="ml-3 border-2 border-blue-500 rounded" type="text" name="content">
+        </div>
+        <div class="mt-10">
+            <button class="bg-gray-600 text-white text-base py-2 px-4 rounded-md" type=”submit” id=”button”>Post</button>
+        </div>
+    </form>
 </div>
 @endsection
