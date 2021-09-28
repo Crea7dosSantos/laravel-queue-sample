50d73d4128092c27cc68e6a7d6ae9ad9d955748d
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-09-29 04:36:21 +0900

    close #8

--- server/resources/views/post/create.blade.php
+++ server/resources/views/post/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div>
-    <h1 class="text-6xl">All Post</h1>
+    <h1 class="text-6xl">Create Post</h1>
 
     <div class="mt-10">
         <a class="bg-gray-600 text-white text-base p-2 rounded-md" href="{{ route('post.create') }}">Create post</a>
