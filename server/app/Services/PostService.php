c1b9d15b38e8779e9a5a8b9a220bfa8100126c17
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-10-22 17:02:15 +0900

    close #19

--- server/app/Services/PostService.php
+++ server/app/Services/PostService.php
@@ -2,11 +2,10 @@
 
 namespace App\Services;
 
-use App\Mail\PostCreated;
+use App\Jobs\SendPostCreatedMail;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
 
 class PostService
 {
@@ -35,24 +34,6 @@ class PostService
 
         $post = Post::create($request->all());
 
-        $this->sendCreatedPostMail($post->email, $post->title);
-    }
-
-    /**
-     * send created post mail
-     *
-     * @param [type] $email
-     * @param [type] $post_title
-     * @return void
-     */
-    private function sendCreatedPostMail($email, $post_title)
-    {
-        Log::debug(__CLASS__ . '::' . __FUNCTION__ . ' called:(' . __LINE__ . ')');
-
-        Log::debug('Started send mail process');
-
-        Mail::to($email)->send(new PostCreated($post_title));
-
-        Log::debug('Completed send mail process');
+        SendPostCreatedMail::dispatch($post->email, $post->title);
     }
 }
